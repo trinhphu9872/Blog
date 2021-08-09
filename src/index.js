de18e2608887197fd32ce7946eebd688567e8435
trinhphu9872 <trinhphu9872@gmail.com>
2021-08-09 16:34:34 +0700

    add extention

--- src/index.js
+++ src/index.js
@@ -7,29 +7,29 @@ const route = require('./resources/routes');
 
 const PORT = 8080;
 // using css and img with Static npm and node-sass
-               app.use(express.static(path.join(__dirname, 'public')));
-               // set views in project
-               app.engine(
-                   '.hbs',
-                   handlebar({
-                       extname: '.hbs',
-                   }),
-               );
-                  app.set('view engine', '.hbs');
-                  app.set('views', path.join(__dirname, 'resources/views'));
-                  // set logger
-                  app.use(morgan('tiny'));
-                  // add middleware
-                  app.use(
-                      express.urlencoded({
-                          extended: true,
-                      }),
-                  );
-                  app.use(express.json());
-                  
-               // Routes init
-               route(app);
-
-app.listen(PORT, () => {
+app.use(express.static(path.join(__dirname, 'public')));
+// set views in project
+app.engine(
+    '.hbs',
+    handlebar({
+        extname: '.hbs',
+    }),
+);
+    app.set('view engine', '.hbs');
+    app.set('views', path.join(__dirname, 'resources/views'));
+    // set logger
+    app.use(morgan('tiny'));
+    // add middleware
+    app.use(
+        express.urlencoded({
+            extended: true,
+        }),
+    );
+    app.use(express.json());
+    
+    // Routes init
+    route(app);
+    
+    app.listen(PORT, () => {
     console.log(`This app render in port local with http://localhost:${PORT}`);
 });
